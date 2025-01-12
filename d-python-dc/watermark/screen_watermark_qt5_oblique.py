9234e0148f70e1042f160fbe94bd36f813093bd3
Excalibra <83846602+Excalibra@users.noreply.github.com>
2025-01-12 16:38:36 +0300

    Update screen_watermark_qt5_oblique.py

--- d-python-dc/watermark/screen_watermark_qt5_oblique.py
+++ d-python-dc/watermark/screen_watermark_qt5_oblique.py
@@ -5,7 +5,28 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QFont
 import hostname_user_ip_date
 
+def create_windows():
+    windows = []
 
+    # Define offsets for each position
+    positions = [
+        ('nw', 50, 50),  # Top left corner, offsets (pady, padx)
+        ('sw', 100, 50),  # Bottom left corner, offsets (pady, padx)
+        ('ne', 50, 150),  # Top right corner, offsets (pady, padx)
+        ('se', 100, 150),  # Bottom right corner, offsets (pady, padx)
+        ('center', 150, 0),  # Center, offsets (pady, padx)
+        ('w', 0, 0),     # Middle left, offsets (pady, padx)
+        ('e', 100, 400),    # Middle right, offsets (pady, padx)
+        ('right_bottom', 100, 100)  # New bottom-right watermark, offsets (pady, padx)
+    ]
+
+    for position, pady, padx in positions:
+        window = WatermarkWindow(position, pady=pady, padx=padx)
+        window.show()
+        windows.append(window)
+
+    return windows
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     windows = create_windows()
