02ec4d15ee24ca1eea6765148d2c184c15ed0ef2
Excalibra <83846602+Excalibra@users.noreply.github.com>
2025-01-12 16:38:12 +0300

    Update screen_watermark_qt5_oblique.py

--- d-python-dc/watermark/screen_watermark_qt5_oblique.py
+++ d-python-dc/watermark/screen_watermark_qt5_oblique.py
@@ -5,3 +5,8 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QFont
 import hostname_user_ip_date
 
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    windows = create_windows()
+    sys.exit(app.exec_())
